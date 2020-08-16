40a289fdf8c0aa9e2d39dd3f8fc4293a04c89b74
nethmi-1997 <63659987+nethmi-1997@users.noreply.github.com>
2020-08-17 05:18:15 +0530

    Nethmi second commit

---
+++ app/src/main/java/com/courierx/courierx/findPackage.java
@@ -0,0 +1,14 @@
+package com.courierx.courierx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class findPackage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_package);
+    }
+}
---
+++ app/src/main/java/com/courierx/courierx/setDelivery.java
@@ -0,0 +1,14 @@
+package com.courierx.courierx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setDelivery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_delivery);
+    }
+}
