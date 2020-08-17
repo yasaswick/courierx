404186052817e8ef8efcbfd4e0405f883aa1c04a
AkilaRanasinghe <akilaranasingha41@gmail.com>
2020-08-17 10:10:09 +0530

    AkilaThirdEdit

---
+++ app/src/main/java/com/courierx/courierx/trackedAndNotPicked.java
@@ -0,0 +1,14 @@
+package com.courierx.courierx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trackedAndNotPicked extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracked_and_not_picked);
+    }
+}
